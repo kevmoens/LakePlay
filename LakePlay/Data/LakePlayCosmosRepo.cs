8e321710be13c46ee84998d967512869d7e011a7
Kevin Moens <kevmoens@yahoo.com>
2024-03-12 21:07:16 -0500

    Add locking to avoid multiple threads updating context at the same time

--- LakePlay/Data/LakePlayCosmosRepo.cs
+++ LakePlay/Data/LakePlayCosmosRepo.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
+using System.Threading;
 
 namespace LakePlay.Data
 {
     public class LakePlayCosmosRepo : ILakePlayRepo<TriviaQuestion>
     {
         private readonly LakePlayCosmosContext _context;
-
+        private SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
         public LakePlayCosmosRepo(LakePlayCosmosContext context)
         {
             _context = context;
         }
         public void Add(TriviaQuestion entity)
         {
+            _semaphore.Wait();
             _context.Questions!.Add(entity);
+            _semaphore.Release();
         }
 
         public void Dispose()
@@ -25,28 +28,42 @@ namespace LakePlay.Data
 
         public async Task<List<TriviaQuestion>> LoadAllAsync()
         {
-            return await _context.Questions!.ToListAsync();
+            await _semaphore.WaitAsync();
+            var resp = await _context.Questions!.ToListAsync();
+            _semaphore.Release();
+            return resp;
         }
 
         public void Remove(TriviaQuestion entity)
         {
+            _semaphore.Wait();
             _context.Questions!.Remove(entity);
+            _semaphore.Release();
         }
 
         public async Task<int> SaveAsync()
         {
-            return await _context.SaveChangesAsync();
+            await _semaphore.WaitAsync();
+            var resp = await _context.SaveChangesAsync();
+            _semaphore.Release();
+            return resp;
         }
 
         public void Update(TriviaQuestion entity)
         {
+            _semaphore.Wait();
             _context.Questions!.Update(entity);
+            _semaphore.Release();
         }
         
         public async Task<List<TriviaQuestion>> Where(Expression<Func<TriviaQuestion, bool>> clause)
         {
+            await _semaphore.WaitAsync();
             var query = _context.Questions!.Where(clause);
-            return await query.ToListAsync();
+            var resp = await query.ToListAsync();
+            _semaphore.Release();
+            return resp;
+
         }
        
     }
